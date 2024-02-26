d49a255830e5d174465dd3b21ab42c92de7aaf59
Caasi2 <caasim353@gmail.com>
2024-02-26 04:13:31 -0300

    Indentation Fix

--- hard_codedPoints.py
+++ hard_codedPoints.py
@@ -22,16 +22,13 @@ points2 = np.float32([
 ])
 
 matrix = cv2.getPerspectiveTransform(points, points2)
-imgOutput = cv2.warpPerspective(img, matrix, (width, height)) 
+imgOutput = cv2.warpPerspective(img, matrix, (width, height))
 
 #for x in range(0, 4):
 #cv2.circle(img, (int(pts1[0][0]), int(pts1[0][1])), 10, (0,0,255), cv2.FILLED)
 
-
 for point in points:
     cv2.circle(img, (int(point[0]), int(point[1])), 10, (0,0,255), cv2.FILLED)
-
-
 
 cv2.namedWindow('My', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('My', 960, 880)
